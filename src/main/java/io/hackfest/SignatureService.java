1c5168a152d69d12ba3378b335dfd1fb2469163a
Brutus5000 <brutus5000@gmx.net>
2023-03-22 11:26:50 +0100

    Proper signing instead of macs

--- src/main/java/io/hackfest/SignatureService.java
+++ src/main/java/io/hackfest/SignatureService.java
@@ -2,43 +2,44 @@ package io.hackfest;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-import javax.crypto.Mac;
 import javax.enterprise.context.ApplicationScoped;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.Security;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.UUID;
 
 @ApplicationScoped
 public class SignatureService {
-    private final static String DEVICE_ID = UUID.randomUUID().toString();
+    private final static String DEVICE_ID = "Shop1_dev1";
     private final static String KEYSTORE_PATH = "keystore.jks";
     private final static String KEYSTORE_PASSWORD = "banana";
 
-    private final Mac mac = Mac.getInstance("HmacSHA256");
+    private final Signature sig = Signature.getInstance("SHA256withRSA");
     private final KeyStore keyStore;
-    private final Key key;
+    private final PrivateKey key;
 
     public SignatureService() throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
         Security.addProvider(new BouncyCastleProvider());
         keyStore = KeyStore.Builder.newInstance(Paths.get(KEYSTORE_PATH).toFile(), new KeyStore.PasswordProtection(KEYSTORE_PASSWORD.toCharArray())).getKeyStore();
-        key = keyStore.getKey("certificate", KEYSTORE_PASSWORD.toCharArray());
+        key = (PrivateKey) keyStore.getKey("certificate", KEYSTORE_PASSWORD.toCharArray());
     }
 
-    public CryptoHeaders buildCryptoHeaders() throws InvalidKeyException {
+    public CryptoHeaders buildCryptoHeaders() throws InvalidKeyException, SignatureException {
         long timestamp = ZonedDateTime.now(ZoneOffset.UTC).toEpochSecond();
         String signatureBase = DEVICE_ID + "::" + timestamp;
 
-        mac.init(key);
-        byte[] signature = mac.doFinal(signatureBase.getBytes(StandardCharsets.UTF_8));
+        sig.initSign(key);
+        sig.update(signatureBase.getBytes(StandardCharsets.UTF_8));
+        byte[] signature = sig.sign();
 
         return new CryptoHeaders(DEVICE_ID, timestamp, signature);
     }
